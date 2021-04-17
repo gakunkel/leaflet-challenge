2c305b674f1b73e104e1b4cb6ef1251b71d9f039
gakunkel <gkunkel@ucsd.edu>
2021-04-16 22:11:19 -0700

    Initialized repository

---
+++ Step_1/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZ2FrdW5rZWwxIiwiYSI6ImNrbjZydGhsOTA5ejYyd296MnA5OGh0aTkifQ.9m40Q_cC0S7yJ8HcqpAnTA";
---
+++ Step_1/static/js/logic.js
@@ -0,0 +1,4 @@
+// This project is to create a map of all earthquakes from the past 7 days using Leaflet/Mapbox
+
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
